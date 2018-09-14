5193bf5a964499548681a15c4a7c6cb65fd5b9e3
Ndande Carleton <ndandeyezai@yahoo.com>
2018-09-14 15:00:04 +0100

    first version,adding footer,header and parallex slideshow

---
+++ index.php
@@ -0,0 +1,241 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="Dist/Bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Dist/Font-Awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="Dist/Pignose/css/pignose.parallaxslider.min.css" />
+    <link rel="stylesheet" href="Dist/Main/css/main.css">
+
+    <title>E-Workspace</title>
+  </head>
+  <body>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed">
+      <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0"> 
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li> 
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+           <button type="button" class="btn btn-outline-success my-2 my-sm-0" style="margin-right: 9px;">Join</button>
+           <button type="button" class="btn btn-outline-success my-2 my-sm-0">SIGN UP</button>
+          
+        </form>
+      </div>
+    </nav>
+    <div id="wrapper">
+      <div id="visual">
+        <div class="slide-visual">
+          <!-- Slide Image Area (1000 x 424) -->
+          <ul class="slide-group">
+            <li><img src="Data/img/visual_slide01.jpg" alt="slide image" /></li>
+            <li><img src="Data/img/visual_slide02.jpg" alt="slide image" /></li>
+            <li><img src="Data/img/visual_slide03.jpg" alt="slide image" /></li>
+            <li><img src="Data/img/visual_slide04.jpg" alt="slide image" /></li>
+            <li><img src="Data/img/visual_slide05.jpg" alt="slide image" /></li>
+            <li><img src="Data/img/visual_slide06.jpg" alt="slide image" /></li>
+          </ul>
+
+          <!-- Slide Description Image Area (316 x 328) -->
+          <div class="script-wrap">
+            <ul class="script-group">
+              <li><div class="inner-script"><img src="Data/img/visual_slide_script01.jpg" alt="thumbnail slider image" /></div></li>
+              <li><div class="inner-script"><img src="Data/img/visual_slide_script02.jpg" alt="thumbnail slider image" /></div></li>
+              <li><div class="inner-script"><img src="Data/img/visual_slide_script03.jpg" alt="thumbnail slider image" /></div></li>
+              <li><div class="inner-script"><img src="Data/img/visual_slide_script04.jpg" alt="thumbnail slider image" /></div></li>
+              <li><div class="inner-script"><img src="Data/img/visual_slide_script05.jpg" alt="thumbnail slider image" /></div></li>
+              <li><div class="inner-script"><img src="Data/img/visual_slide_script06.jpg" alt="thumbnail slider image" /></div></li>
+            </ul>
+            <div class="slide-controller">
+              <a href="#" class="btn-prev"><img src="Data/img/btn_prev.png" alt="prev slide" /></a>
+              <a href="#" class="btn-play"><img src="Data/img/btn_play.png" alt="start slide" /></a>
+              <a href="#" class="btn-pause"><img src="Data/img/btn_pause.png" alt="pause slide" /></a>
+              <a href="#" class="btn-next"><img src="Data/img/btn_next.png" alt="next slide" /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <section class="search-box">
+        <form action="" method="">        
+          <input type="search" placeholder="What are you looking for?">         
+          <button>Search</button>
+        </form>
+      </section>
+    </div>
+    <div class="submit" id="h2" data-toggle="buttons">
+        <ul class="list-inline">
+          <li>
+            <button type="checkbox" checked autocomplete="off" class="btn btn-outline-primary btn-lg active">FREELANCER?</button>
+            <button type="checkbox" checked autocomplete="off" class="btn btn-outline-* btn-lg active" style="background-color: #212529; color: white;">EMPLOYER?</button>
+            <button type="checkbox" checked autocomplete="off" class="btn btn-outline-* btn-lg active" style="background-color: #28a745; color: white; padding-right: 40px; padding-left: 40px;">BOTH?</button>
+          </li>
+        </ul>
+    </div>
+    <!-- Footer -->
+    <footer class="page-footer font-small unique-color-dark footer-container">
+      <div class="site-foo">
+        <div class="container">
+
+          <!-- Grid row-->
+          <div class="row py-4 d-flex align-items-center">
+
+            <!-- Grid column -->
+            <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+              <h6 class="mb-0">Get connected with us on social networks!</h6>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+              <!-- Facebook -->
+              <a class="fb-ic">
+                <i class="fa fa-facebook white-text mr-4"> </i>
+              </a>
+              <!-- Twitter -->
+              <a class="tw-ic">
+                <i class="fa fa-twitter white-text mr-4"> </i>
+              </a>
+              <!-- Google +-->
+              <a class="gplus-ic">
+                <i class="fa fa-google-plus white-text mr-4"> </i>
+              </a>
+              <!--Linkedin -->
+              <a class="li-ic">
+                <i class="fa fa-linkedin white-text mr-4"> </i>
+              </a>
+              <!--Instagram-->
+              <a class="ins-ic">
+                <i class="fa fa-instagram white-text"> </i>
+              </a>
+
+            </div>
+            <!-- Grid column -->
+
+          </div>
+          <!-- Grid row-->
+
+        </div>
+      </div>
+      <!-- Footer Links -->
+      <div class="container text-center text-md-left mt-5 ">
+
+        <!-- Grid row -->
+        <div class="row mt-3">
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+            <!-- Content -->
+            <h6 class="text-uppercase font-weight-bold">E-Workspace</h6>
+            <hr class="hr_ll" style="width: 60px;">
+            <!-- <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit.</p> -->
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+            <!-- Links -->
+            <h6 class="text-uppercase font-weight-bold">Product</h6>
+            <hr class="hr_ll" style="width: 60px;">
+            <p>
+              <a href="#!">About</a>
+            </p>
+            <p>
+              <a href="#!">Team</a>
+            </p>
+            <p>
+              <a href="#!">Media</a>
+            </p>
+            <p>
+              <a href="#!">Categories</a>
+            </p>
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+            <!-- Links -->
+            <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+            <hr class="hr_ll" style="width: 60px;">
+            <p>
+              <a href="#!">Your Account</a>
+            </p>
+            <p>
+              <a href="#!">How it works</a>
+            </p>
+            <p>
+              <a href="#!">Help</a>
+            </p>
+            <p>
+              <a href="#!">Trust and safety</a>
+            </p>
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+            <!-- Links -->
+            <h6 class="text-uppercase font-weight-bold">Contact</h6>
+            <hr class="hr_ll" style="width: 60px;">
+            <p>
+              <i class="fa fa-home mr-3"></i> Yaounde, Cameroon</p>
+            <p>
+              <i class="fa fa-envelope mr-3"></i> Eworkspace@yahoo.com</p>
+            <p>
+              <i class="fa fa-phone mr-3"></i> + 237 508 048 08</p>
+            <p>
+              <i class="fa fa-print mr-3"></i> + 237 508 048 08</p>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+      </div>
+      <!-- Footer Links -->
+      <!-- Copyright -->
+      <div class="footer-copyright text-center py-3">© 2018 Copyright:
+        <a href="https://mdbootstrap.com/bootstrap-tutorial/"> Camsoft group</a>
+      </div>
+      <!-- Copyright -->
+    </footer>
+    
+    <script type="text/javascript" src="Dist/Bootstrap/js/bootstrap.min.js" ></script>
+    <script type="text/javascript" src="Dist/Bootstrap/js/jquery.latest.min.js"></script>
+    <script type="text/javascript" src="Dist/Bootstrap/js/jquery.easing.js"></script>
+    <script type="text/javascript" src="Dist/Pignose/js/pignose.parallaxslider.min.js"></script>
+    <script type="text/javascript">
+    //<![CDATA[
+      $(window).load(function() {
+        $('#visual').pignoseParallaxSlider({
+          play    : '.btn-play',
+          pause   : '.btn-pause',
+          next    : '.btn-next',
+          prev    : '.btn-prev'
+        });
+      });
+    //]]>
+    </script>
+  </body>
+</html>
